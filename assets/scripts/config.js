b93f35fb6f950c7ea33923e35b961f0400c650bf
rlarsonil71 <ron.larson@libertymutual.com>
2017-04-25 20:20:15 -0400

    Updated project URL to point to deployed Heroku API URL
    Updated config.apiOrigins production URL to:
    'https://hidden-cliffs-39330.herokuapp.com/' set up using instructions
    in the https://github.com/ga-wdi-boston/rails-heroku-setup-guide repo.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    // production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://hidden-cliffs-39330.herokuapp.com/'
   }
 }
 
